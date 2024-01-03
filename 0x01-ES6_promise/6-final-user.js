6413544d8f410dfa3f2a8b7b1a059598ad46b8c9
oumatoulacen <oumatoulacen@gmail.com>
2024-01-03 22:26:16 +0100

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return [await signUpUser(firstName, lastName), await uploadPhoto(fileName)];
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  const [userResult, photoResult] = await Promise.all([userPromise, photoPromise]);
+
+  return [
+    { status: 'fulfilled', value: userResult },
+    { status: 'fulfilled', value: photoResult },
+  ];
 }
