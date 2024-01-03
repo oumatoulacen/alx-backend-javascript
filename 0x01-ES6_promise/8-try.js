9674b51af8d6fb1439f5eae5fa44e2b38d5c6775
oumatoulacen <oumatoulacen@gmail.com>
2024-01-03 23:41:37 +0100

    Throw error / try catch

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,11 +1,8 @@
 export default function divideFunction(numerator, denominator) {
   try {
-    if (denominator === 0) {
-      throw Error('cannot divide by 0');
-    } else {
-      return numerator / denominator;
-    }
+    if (denominator === 0) throw new Error('cannot divide by 0');
+    return numerator / denominator;
   } catch (e) {
-    return new Error(e);
+    throw Error('cannot divide by 0');
   }
 }
