962aa3bda72ef093795c9c2e467c6429ce74ff88
oumatoulacen <oumatoulacen@gmail.com>
2024-01-04 00:44:50 +0100

    Await / Async

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,10 +1,18 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function firstasyncUploadUser() {
-  const user = await createUser();
-  const photo = await uploadPhoto();
-  return {
-    photo: user,
-    user: photo,
-  };
+  try {
+    const user = await createUser();
+    const photo = await uploadPhoto();
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    // console.error('An error occurred:', error.message);
+    return {
+      photo: null,
+      user: null,
+    };
+  }
 }
