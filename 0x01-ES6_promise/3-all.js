4bf9ce1d59c96e7987a3ce52d380c09d22c36ef1
oumatoulacen <oumatoulacen@gmail.com>
2024-01-03 19:28:39 +0100

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser} from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto, createUser])
+    .then(() => {
+      console.log('body firstName lastName');
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
+}
