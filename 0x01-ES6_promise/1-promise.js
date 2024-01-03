5b8f3be6a2e25777ac43cc201b86128ddcb825d5
oumatoulacen <oumatoulacen@gmail.com>
2024-01-03 14:57:18 +0100

    Don't make a promise...if you know you can't keep it

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -4,7 +4,7 @@ export default function getFullResponseFromAPI(success) {
       resolve({ status: 200, body: 'Success' });
     } else {
       // eslint-disable-next-line prefer-promise-reject-errors
-      reject(Error('The fake API is not working currently'));
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
