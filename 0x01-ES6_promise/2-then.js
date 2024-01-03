bae7e5601ced66d35598b38666c4f935866ab4e7
oumatoulacen <oumatoulacen@gmail.com>
2024-01-03 17:36:04 +0100

    Catch me if you can

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,12 @@
 export default function handleResponseFromAPI(promise) {
   promise
-    .then(() => {
+    // eslint-disable-next-line no-unused-vars
+    .then((_result) => {
       console.log('Got a response from the API');
       return { status: 200, body: 'success' };
     })
-    .catch(() => {
+    // eslint-disable-next-line no-unused-vars
+    .catch((_error) => {
       console.log('Got a response from the API');
       return new Error();
     });
