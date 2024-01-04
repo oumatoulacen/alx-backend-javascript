e7ef704cbe77b411dc38379b8bce1af7b45a51eb
oumatoulacen <oumatoulacen@gmail.com>
2024-01-04 23:16:38 +0100

    A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,11 +1,12 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable max-len */
+/**
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = typeof name === 'string' ? name : new TypeError('Name must be a string');
-    this._length = typeof length === 'number' ? length : new TypeError('Length must be a number');
-    this._students = Array.isArray(students) ? students : new TypeError('Students must be an array');
+    this._name = typeof name === 'string' ? name : new Error('name must be a string');
+    this._length = typeof length === 'number' ? length : new Error('Length must be a number');
+    this._students = Array.isArray(students) ? students : new Error('students must be an array');
   }
 
   get name() { return this._name; }
@@ -20,61 +21,60 @@ export default class HolbertonCourse {
   get students() { return this._students; }
 
   set students(value) { this._students = Array.isArray(value) ? value : new Error('students must be an array'); }
+} */
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = this._validateString(name, 'Name');
+    this._length = this._validateNumber(length, 'Length');
+    this._students = this._validateArray(students, 'Students');
+  }
+
+  // Getter and setter for name
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = this._validateString(newName, 'Name');
+  }
+  
+  // Getter and setter for length
+  get length() {
+    return this._length;
+  }
+  
+  set length(newLength) {
+    this._length = this._validateNumber(newLength, 'Length');
+  }
+  
+  // Getter and setter for students
+  get students() {
+    return this._students;
+  }
+  
+  set students(newStudents) {
+    this._students = this._validateArray(newStudents, 'Students');
+  }
+  
+  // Validation functions
+  _validateString(value, propertyName) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${propertyName} must be a string`);
+    }
+    return value;
+  }
+  
+  _validateNumber(value, propertyName) {
+    if (typeof value !== 'number') {
+      throw new TypeError(`${propertyName} must be a number`);
+    }
+    return value;
+  }
+  
+  _validateArray(value, propertyName) {
+    if (!Array.isArray(value)) {
+      throw new TypeError(`${propertyName} must be an array`);
+    }
+    return value;
+  }
 }
-
-// export default class HolbertonCourse {
-//   constructor(name, length, students) {
-//     this._name = this._validateString(name, 'Name');
-//     this._length = this._validateNumber(length, 'Length');
-//     this._students = this._validateArray(students, 'Students');
-//   }
-
-//   // Getter and setter for name
-//   get name() {
-//     return this._name;
-//   }
-
-//   set name(newName) {
-//     this._name = this._validateString(newName, 'Name');
-//   }
-  
-//   // Getter and setter for length
-//   get length() {
-//     return this._length;
-//   }
-  
-//   set length(newLength) {
-//     this._length = this._validateNumber(newLength, 'Length');
-//   }
-  
-//   // Getter and setter for students
-//   get students() {
-//     return this._students;
-//   }
-  
-//   set students(newStudents) {
-//     this._students = this._validateArray(newStudents, 'Students');
-//   }
-  
-//   // Validation functions
-//   _validateString(value, propertyName) {
-//     if (typeof value !== 'string') {
-//       throw new TypeError(`${propertyName} must be a string`);
-//     }
-//     return value;
-//   }
-  
-//   _validateNumber(value, propertyName) {
-//     if (typeof value !== 'number') {
-//       throw new TypeError(`${propertyName} must be a number`);
-//     }
-//     return value;
-//   }
-  
-//   _validateArray(value, propertyName) {
-//     if (!Array.isArray(value)) {
-//       throw new TypeError(`${propertyName} must be an array`);
-//     }
-//     return value;
-//   }
-// }
