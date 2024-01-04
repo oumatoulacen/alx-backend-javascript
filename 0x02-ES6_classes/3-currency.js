84273d08db307bbb1bdb2dd8e8bfc01e29f71f73
oumatoulacen <oumatoulacen@gmail.com>
2024-01-05 00:01:15 +0100

    Methods, static methods, computed methods names..... MONEY

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,10 +1,38 @@
 export default class Currency {
   constructor(code, name) {
-    this._code = code;
-    this._name = name;
+    this._code = this._validateString(code, 'Code');
+    this._name = this._validateString(name, 'Name');
   }
 
+  // Getter and setter for code
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this._code = this._validateString(newCode, 'Code');
+  }
+
+  // Getter and setter for name
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = this._validateString(newName, 'Name');
+  }
+
+  // Method to display full currency information
   displayFullCurrency() {
-    return `${this._name} (${this._code}) `;
+    return `${this._name} (${this._code})`;
+  }
+
+  // Validation function for strings
+  // eslint-disable-next-line class-methods-use-this
+  _validateString(value, propertyName) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${propertyName} must be a string`);
+    }
+    return value;
   }
 }
