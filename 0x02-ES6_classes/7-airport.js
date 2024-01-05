eac9bda99bd998e2039d8756a4c484af2b7006a9
oumatoulacen <oumatoulacen@gmail.com>
2024-01-05 02:04:30 +0100

    inheritance/abstract class

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -4,19 +4,19 @@ export default class Airport {
     this._code = code;
   }
 
-  get name() {
-    return this._name;
-  }
-
   set name(name) {
     this._name = name;
   }
 
-  get code() {
-    return this._code;
+  get name() {
+    return this._name;
   }
 
   set code(code) {
     this._code = code;
+  }
+
+  get code() {
+    return this._code;
   }
 }
