c4d5757807f26a686a704ec33f7d83947af85237
oumatoulacen <oumatoulacen@gmail.com>
2024-01-08 22:26:33 +0100

    Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,6 @@
+export default function getListStudents() {
+  return ([
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstname: 'James', location: 'Columbia' },
+    { id: 5, firstname: 'Serena', location: 'San Francisco' }]);
+}
