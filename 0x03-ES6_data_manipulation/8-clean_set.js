47ebf6e512725c6c3223b9d3f811839831114c66
oumatoulacen <oumatoulacen@gmail.com>
2024-01-09 18:37:02 +0100

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
-export default function cleanSet(set, str) {
-  if (str === '' || !str) return '';
+export default function cleanSet(set, startString) {
+  if (startString === '' || !startString) return '';
   const arr = Array.from(set);
-  return arr.filter((el) => (el.startsWith(str))).map((el) => el.slice(str.length)).join('-');
+  return arr.filter((el) => (el.startsWith(startString))).map((el) => el.slice(startString.length)).join('-');
 }
