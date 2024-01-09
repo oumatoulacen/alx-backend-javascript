a0e8138dadd49515e09cbf88f4892590485988c9
oumatoulacen <oumatoulacen@gmail.com>
2024-01-09 12:41:22 +0100

    new ArrayBuffer of the specified length and sets the Int8 value at the given position

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  if (position < 0 || position > buffer.length) {
+  if (position < 0 || position > buffer.byteLength) {
     throw new Error('Position outside range');
   }
   const array = new Int8Array(buffer);
