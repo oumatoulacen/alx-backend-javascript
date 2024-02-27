39834340ad3b9f8ff209adcf6d2c6a9142302e3b
oumatoulacen <oumatoulacen@gmail.com>
2024-02-27 23:55:32 +0100

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+#!/usr/bin/env node
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (data) => {
+  const input = data.trim();
+  console.log('Your name is:', input);
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
