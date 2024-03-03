70839f12b52c36549875b7974626df992f253ef3
oumatoulacen <oumatoulacen@gmail.com>
2024-03-03 01:38:12 +0100

    Reading a file asynchronously with Node JS~

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,1 +1,29 @@
-#!/usr/bin/node
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+      } else {
+        const lines = data.split('\n').filter((line) => line.length > 0);
+        lines.shift();
+        console.log(`Number of students: ${lines.length}`);
+        const fields = {};
+        lines.forEach((line) => {
+          const student = line.split(',');
+          if (!fields[student[3]]) fields[student[3]] = [];
+          fields[student[3]].push(student[0]);
+        });
+        for (const field in fields) {
+          if (field && fields[field]) {
+            console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+          }
+        }
+        resolve();
+      }
+    });
+  });
+}
+
+module.exports = countStudents;
