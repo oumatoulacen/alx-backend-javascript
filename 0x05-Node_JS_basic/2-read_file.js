fab27d499420a6b91cc31c7ad7a7e799ae2fc7e1
oumatoulacen <oumatoulacen@gmail.com>
2024-03-03 01:01:56 +0100

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,1 +1,30 @@
-#!/usr/bin/node
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    let lines = data.split('\n');
+    lines = lines.slice(1, lines.length);
+    const fields = {};
+    let count = 0;
+    for (const line of lines) {
+      if (line !== '') {
+        count += 1;
+        const student = line.split(',');
+        if (!fields[student[3]]) fields[student[3]] = [];
+        fields[student[3]].push(student[0]);
+      }
+    }
+    console.log(`Number of students: ${count}`);
+    for (const field in fields) {
+      if (field) {
+        const list = fields[field];
+        console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+      }
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
