26a7d14bc0c1f42a77e387e31e10ce8830ffa9ce
oumatoulacen <oumatoulacen@gmail.com>
2024-03-04 15:34:36 +0100

    Combining descriptions

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+function calculateNumber(a, b, type) {
+    if (type === 'SUM') return Math.round(a) + Math.round(b);
+    if (type === 'SUBTRACT') return Math.round(a) - Math.round(b);
+    if (type === 'DIVIDE') {
+      if (Math.round(b) === 0) return 'Error';
+      return Math.round(a) / Math.round(b);
+    }
+  }
+  
+  module.exports = calculateNumber;
+  
--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,1 +1,32 @@
-#!/usr/bin/node
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', function() {
+    describe('type SUM', function() {
+        it('should return 4', function() {
+            assert.strictEqual(calculateNumber(1, 3, 'SUM'), 4);
+        });
+        it('should return 5', function() {
+            assert.strictEqual(calculateNumber(1.4, 3.5, 'SUM'), 5);
+        });
+    });
+    describe('type SUBTRACT', function() {
+        it('should return 2', function() {
+            assert.strictEqual(calculateNumber(3, 1, 'SUBTRACT'), 2);
+        });
+        it('should return 3', function() {
+            assert.strictEqual(calculateNumber(3.5, 1.4, 'SUBTRACT'), 3);
+        });
+    });
+    describe('type DIVIDE', function() {
+        it('should return 5', function() {
+            assert.strictEqual(calculateNumber(10, 2, 'DIVIDE'), 5);
+        });
+        it('should return 4', function() {
+            assert.strictEqual(calculateNumber(12, 2.5, 'DIVIDE'), 4);
+        });
+        it('should return Error', function() {
+            assert.strictEqual(calculateNumber(10, 0, 'DIVIDE'), 'Error');
+        });
+    });
+});
