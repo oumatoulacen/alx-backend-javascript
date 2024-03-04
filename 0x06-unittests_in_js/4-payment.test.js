db786c8e7c25df6a858ecef9994371cacc83f24f
oumatoulacen <oumatoulacen@gmail.com>
2024-03-04 23:11:13 +0100

    stubs

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -6,9 +6,12 @@ const sinon = require('sinon');
 
 describe('sendPaymentRequestToApi', () => {
   it('should call calculateNumber', () => {
+    const spy = sinon.spy(console, 'log');
     const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
     sendPaymentRequestToApi(100, 20);
+    expect(spy.calledOnceWithExactly('The total is: 10')).to.be.true;
     expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    spy.restore();
     calculateNumberStub.restore();
   });
 });
