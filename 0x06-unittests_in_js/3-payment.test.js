b221b96a68e25aafe71ade7866d4aa7e75edb778
oumatoulacen <oumatoulacen@gmail.com>
2024-03-04 21:58:43 +0100

    Spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,13 +1,12 @@
 const sendPaymentRequestToApi = require('./3-payment');
-const chai = require('chai');
-const expect = chai.expect;
+const { expect } = require('chai');
+
 const sinon = require('sinon');
 
 describe('sendPaymentRequestToApi', function() {
   it('should log the correct result', function() {
-    const spy = sinon.spy('Utils', 'calculateNumber');
+    const spy = sinon.spy(console, 'log');
     sendPaymentRequestToApi(100, 20);
-    expect(spy.calledOnce).to.be.true;
-    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(spy.calledWith('The total is: 120')).to.be.true;
   });
 });
