6f67e96ebd7488a86cc2ebf867fa3e4e8d4dd4b6
oumatoulacen <oumatoulacen@gmail.com>
2024-03-05 01:37:57 +0100

    stubs

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,8 +1,9 @@
 const Utils = require('./utils');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${result}`);
+  const sum = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${sum}`);
+  // Make API request with the total amount
 }
 
 module.exports = sendPaymentRequestToApi;
