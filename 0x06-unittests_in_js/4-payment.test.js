25cd5e1ce7b23b8b9d7b8d74fb474138e39c89a5
oumatoulacen <oumatoulacen@gmail.com>
2024-03-05 01:18:28 +0100

    stubs

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -3,13 +3,16 @@ const { expect } = require('chai');
 const Utils = require('./utils.js');
 
 const sinon = require('sinon');
+const assert = require("assert");
+
 
 describe('sendPaymentRequestToApi', () => {
-  it('should call calculateNumber', () => {
-    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber')
-    calculateNumberStub.withArgs('SUM', 100, 20).returns(120);
-    sendPaymentRequestToApi(100, 20);
-    expect(calculateNumberStub.calledOnceWith('SUM', 100, 20)).to.be.true;
-    calculateNumberStub.restore();
-  });
+    it("check that Utils.calculateNumber is stubbed", function() {
+        const spy = sinon.spy(console, "log");
+        const stub = sinon.stub(Utils, "calculateNumber").returns(10);
+        sendPaymentRequestToApi(100, 20);
+    
+        assert(spy.withArgs("The total is: 10").calledOnce);
+        assert(stub.withArgs("SUM", 100, 20).calledOnce);
+        });
 });
