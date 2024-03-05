92a4834cca2e9a0561d5a05b6b043ec5d46ed2ff
oumatoulacen <oumatoulacen@gmail.com>
2024-03-05 18:39:16 +0100

    Hooks

--- 0x06-unittests_in_js/5-payment.js
+++ 0x06-unittests_in_js/5-payment.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    const val = Utils.calculateNumber('SUM', totalAmount, totalShipping)
+    console.log(`The total is: ${val}`)
+}
+
+module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,1 +1,26 @@
-#!/usr/bin/node
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./5-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function() {
+  let consoleLogSpy;
+
+  beforeEach(function() {
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(function() {
+    consoleLogSpy.restore();
+  });
+
+  it('should log correct message for totalAmount = 100 and totalShipping = 20', function() {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+  });
+
+  it('should log correct message for totalAmount = 10 and totalShipping = 10', function() {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+  });
+});
